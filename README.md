986579c2c7139b542fa61bcf4fdcd023b33bf39d
taraskl <taras.kliuchnyk@gmail.com>
2019-06-24 13:03:58 +0300

    create project

---
+++ README.md
@@ -0,0 +1,22 @@
+# Final Project
+
+Education project from edX HarvardX course: CS50's Web Programming with Python and JavaScript
+
+Online store
+
+Requirements:
+
+Must be mobile-responsive.
+
+Landing page: Page where customers can subscribe to a newsletter about opening a store.  Subscribers should to be saved and showed on Django Admin.
+
+Adding Items: Using Django Admin, site administrators should be able to add, update, and remove items on the main page, add and remove categories, add and remove item to categories, add and remove photos to item.
+
+Shopping Cart: Logging not needed, the customers can add and delete items to their virtual “shopping cart” without refreshing a page. The contents of the shopping should be saved even if a user closes the window.
+
+Make Order: Once there is at least one item in a user’s shopping cart, they should be able to place an order.
+
+Manage Orders: Site administrators on Django Admin can view orders, change order status(New, In work, Canseled, Done), add comments and adress to oreders.
+
+
+Description: Project consist with three apps: landig, products, orders each of one has his own models, urls and views. Static files have two versions for developing and product version.
---
+++ products/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "%s" % self.name
+
+    class Meta:
+        verbose_name = 'Product category'
+        verbose_name_plural = 'Product categories'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=64, blank=True, null=True, default=None)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    discount = models.IntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, blank=True, null=True, default=None)
+    short_description = models.TextField(blank=True, null=True, default=None)
+    description = models.TextField(blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "%s, %s" % (self.price, self.name)
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, blank=True, null=True, default=None)
+    image = models.ImageField(upload_to='products_images/')
+    is_main = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "%s" % self.id
+
+    class Meta:
+        verbose_name = 'Photo'
+        verbose_name_plural = 'Photos'
